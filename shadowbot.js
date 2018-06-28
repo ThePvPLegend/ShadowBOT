d0bab40703859824d65dd28294f9412192e3b48b
ThePvPLegend <40651808+ThePvPLegend@users.noreply.github.com>
2018-06-28 07:08:11 +0300

    Create shadowbot.js

---
+++ shadowbot.js
@@ -0,0 +1,6 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('MzYxODUyMTA0NDU0NzAxMDU2.Dbfd9A.bfUtiQ2uIzeSVTFqBSBlKZ661nk');
+});
